f8f62f0ef14da9fd136cc482cecb9a2ce7c831c4
metaspook <metaspook@gmail.com>
2021-11-15 18:08:15 +0600

    Add navigation from home

--- lib/pages/number_table.dart
+++ lib/pages/number_table.dart
@@ -12,49 +12,52 @@ class NumberTable extends StatefulWidget {
 }
 
 class _NumberTableState extends State<NumberTable> {
+  // Fetch content from the json file
+  // Tried to but couldn't get it to work
+  // couldn't set value on List<Map<String, dynamic>> _numberTableList
   static const List<Map<String, dynamic>> _numberTableList = [
     {
-      'id': 1,
-      'first': ['1', '3', '5', '7', '9', '11', '13', '15'],
-      'second': ['17', '19', '21', '23', '25', '27', '29', '31'],
-      'third': ['33', '35', '37', '39', '41', '43', '45', '47'],
-      'fourth': ['49', '51', '53', '55', '57', '59', '61', '63']
+      "id": 1,
+      "first": [11, 3, 5, 7, 9, 11, 13, 15],
+      "second": [17, 19, 21, 23, 25, 27, 29, 31],
+      "third": [33, 35, 37, 39, 41, 43, 45, 47],
+      "fourth": [49, 51, 53, 55, 57, 59, 61, 63]
     },
     {
-      'id': 2,
-      'first': ['2', '3', '6', '7', '10', '11', '14', '15'],
-      'second': ['18', '19', '22', '23', '26', '27', '30', '31'],
-      'third': ['34', '35', '38', '39', '42', '43', '46', '47'],
-      'fourth': ['50', '51', '54', '55', '58', '59', '62', '63'],
+      "id": 2,
+      "first": [2, 3, 6, 7, 10, 11, 14, 15],
+      "second": [18, 19, 22, 23, 26, 27, 30, 31],
+      "third": [34, 35, 38, 39, 42, 43, 46, 47],
+      "fourth": [50, 51, 54, 55, 58, 59, 62, 63]
     },
     {
-      'id': 3,
-      'first': ['4', '5', '6', '7', '12', '13', '14', '15'],
-      'second': ['20', '21', '22', '23', '28', '29', '30', '31'],
-      'third': ['36', '37', '38', '39', '44', '45', '46', '47'],
-      'fourth': ['52', '53', '54', '55', '60', '61', '62', '63'],
+      "id": 3,
+      "first": [4, 5, 6, 7, 12, 13, 14, 15],
+      "second": [20, 21, 22, 23, 28, 29, 30, 31],
+      "third": [36, 37, 38, 39, 44, 45, 46, 47],
+      "fourth": [52, 53, 54, 55, 60, 61, 62, 63]
     },
     {
-      'id': 4,
-      'first': ['8', '9', '10', '11', '12', '13', '14', '15'],
-      'second': ['24', '25', '26', '27', '28', '29', '30', '31'],
-      'third': ['40', '41', '42', '43', '44', '45', '46', '47'],
-      'fourth': ['56', '57', '58', '59', '60', '61', '62', '63'],
+      "id": 4,
+      "first": [8, 9, 10, 11, 12, 13, 14, 15],
+      "second": [24, 25, 26, 27, 28, 29, 30, 31],
+      "third": [40, 41, 42, 43, 44, 45, 46, 47],
+      "fourth": [56, 57, 58, 59, 60, 61, 62, 63]
     },
     {
-      'id': 5,
-      'first': ['16', '17', '18', '19', '20', '21', '22', '23'],
-      'second': ['24', '25', '26', '27', '28', '29', '30', '31'],
-      'third': ['48', '49', '50', '51', '52', '53', '54', '55'],
-      'fourth': ['56', '57', '58', '59', '60', '61', '62', '63'],
+      "id": 5,
+      "first": [16, 17, 18, 19, 20, 21, 22, 23],
+      "second": [24, 25, 26, 27, 28, 29, 30, 31],
+      "third": [48, 49, 50, 51, 52, 53, 54, 55],
+      "fourth": [56, 57, 58, 59, 60, 61, 62, 63]
     },
     {
-      'id': 6,
-      'first': ['32', '33', '34', '35', '36', '37', '38', '39'],
-      'second': ['40', '41', '42', '43', '44', '45', '46', '47'],
-      'third': ['48', '49', '50', '51', '52', '53', '54', '55'],
-      'fourth': ['56', '57', '58', '59', '60', '61', '62', '63'],
-    },
+      "id": 6,
+      "first": [32, 33, 34, 35, 36, 37, 38, 39],
+      "second": [40, 41, 42, 43, 44, 45, 46, 47],
+      "third": [48, 49, 50, 51, 52, 53, 54, 55],
+      "fourth": [56, 57, 58, 59, 60, 61, 62, 63]
+    }
   ];
   late Widget _numberTableWidget;
   String _outText = '';
@@ -92,7 +95,7 @@ class _NumberTableState extends State<NumberTable> {
                     Center(
                       heightFactor: 1.5,
                       child: Text(
-                        data[key][i],
+                        data[key][i].toString(),
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontFamily: 'Consolas'),
@@ -122,7 +125,7 @@ class _NumberTableState extends State<NumberTable> {
     int sum = 0;
     for (var i = 0; i < _numberTableList.length; i++) {
       if (_numberTableCheckbox[i] == true) {
-        sum += int.tryParse(_numberTableList[i]['first'][0])!;
+        sum += _numberTableList[i]['first'][0] as int;
       }
     }
     setState(() => _outText = sum.toString());
@@ -144,7 +147,6 @@ class _NumberTableState extends State<NumberTable> {
 
   @override
   Widget build(BuildContext context) {
-    // print(_numberTableList[0]['first'][0]);
     // final size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(title: Text(widget.title)),
@@ -152,7 +154,35 @@ class _NumberTableState extends State<NumberTable> {
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: SingleChildScrollView(
             child: Column(children: <Widget>[
-              SizedBox(height: 10),
+              SizedBox(height: 15),
+              Container(
+                padding: EdgeInsets.all(5),
+                width: 500,
+                height: 105,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  border: Border.all(
+                    color: Colors.grey,
+                    // width: 5,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  """
+RULES:
+* Hold a secret number from 1 to 63.
+* Select the tables below containing
+  the secret number you're holding.
+* Press Submit to get the secret number.
+""",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Consolas',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
               _numberTableWidget,
               SizedBox(height: 5),
               Row(
@@ -191,7 +221,7 @@ class _NumberTableState extends State<NumberTable> {
               ),
               SizedBox(height: 20),
               Center(
-                  child: Text('Hidden Number',
+                  child: Text('Secret Number',
                       style: TextStyle(
                           fontSize: 20, fontWeight: FontWeight.bold))),
               SizedBox(height: 10),
@@ -206,8 +236,10 @@ class _NumberTableState extends State<NumberTable> {
                   ),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: Text(_outText,
-                    style: TextStyle(fontFamily: 'Consolas', fontSize: 19)),
+                child: Center(
+                  child: Text(_outText,
+                      style: TextStyle(fontFamily: 'Consolas', fontSize: 50)),
+                ),
               ),
             ]),
           ),
---
+++ lib/widgets/items.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final int index;
+  final String title;
+  final Function func;
+
+  const ItemWidget(
+      {Key? key, required this.index, required this.title, required this.func})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Colors.primaries[index % Colors.primaries.length];
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: color.withOpacity(0.5),
+              blurRadius: 10,
+              spreadRadius: 5,
+            ),
+          ],
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        width: 50,
+        height: 30,
+        // color: color.withOpacity(0.5),
+        child: MaterialButton(
+          onPressed: () => func(),
+          // tileColor: color.withOpacity(0.3),
+          child: Text(title,
+              style: const TextStyle(fontSize: 15, color: Colors.white)),
+        ),
+      ),
+    );
+  }
+}
